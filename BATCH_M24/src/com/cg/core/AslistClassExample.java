9c94377b1db7a84c1e010c71c7dafd27a32543e7
Samadhan <Samadhan@DESKTOP-6AMIRQ3>
2022-08-25 18:43:15 +0530

    aslist() Example

---
+++ BATCH_M24/src/com/cg/core/AslistClassExample.java
@@ -0,0 +1,16 @@
+package com.cg.core;
+
+import java.util.Arrays;
+
+public class AslistClassExample {
+
+	public static void main(String[] args) {
+		
+			int intArr[] = { 10, 20, 15, 22, 35 };
+			System.out.println("Integer Array as List: "
+	                + Arrays.asList(intArr));
+		}
+
+	}
+
+
