09974b380a4a533cbf8d4a792d1f9606906863f3
Samadhan <Samadhan@DESKTOP-6AMIRQ3>
2022-08-31 15:29:36 +0530

    Java Final Class

---
+++ BATCH_M24/src/com/cg/finalkeyword/Bike.java
@@ -0,0 +1,15 @@
+package com.cg.finalkeyword;
+
+public class Bike {}
+
+	class Shine1 extends Bike{  
+		  void run(){System.out.println("running safely with 100kmph");}  
+
+		  public static void main(String args[]){  
+		  Shine1 shine= new Shine1();  
+		  shine.run();  
+
+	}
+
+}
+//	If you make any class as final, you cannot extend it. 
