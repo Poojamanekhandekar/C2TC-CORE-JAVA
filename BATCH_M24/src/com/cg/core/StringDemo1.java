c6916e72fb9ecff07e3f68249bbe2f849a7921b9
Samadhan <Samadhan@DESKTOP-6AMIRQ3>
2022-08-31 15:05:05 +0530

    String_Demo1

---
+++ BATCH_M24/src/com/cg/core/StringDemo1.java
@@ -0,0 +1,28 @@
+package com.cg.core;
+
+public class StringDemo1 {
+
+	public static void main(String[] args) {
+//		String s1=new String("pooja");
+//		String s2=new String("pooja");
+//		String s3="pooja";
+//		String s4="pooja";
+
+
+		String s1= new String("POOJA");
+		String s2= new String("POOJA");
+		String s3= new String("POO");
+
+//		System.out.println(s1.equals(s2)); //for password	
+//		System.out.println(s1.equalsIgnoreCase(s2));//for username
+
+
+//		System.out.println(s1==s2);
+
+	System.out.println(s1.compareTo(s2));
+	System.out.println(s1.compareTo(s3));
+	}
+
+}
+
+	
