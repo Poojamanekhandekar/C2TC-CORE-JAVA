2d306262274d1e2e585eaf56a682383499bdade5
Samadhan <Samadhan@192.168.0.11>
2022-09-05 16:27:54 +0530

    homework

--- BATCH_M24/src/com/cg/inheritanceex/InheritanceEx3.java
+++ BATCH_M24/src/com/cg/inheritanceex/InheritanceEx3.java
@@ -5,14 +5,14 @@ void Intrest(){System.out.println("2%");}
 class PersonalLoan1 extends bank1loan{  
 void IntrestPL(){System.out.println("5%");}  
 }  
-class carloan extends bank1loan{  
+class Carloan extends bank1loan{  
 void IntrestCL(){System.out.println("8%");}  
 }  
 
 public class InheritanceEx3 {
 
 	public static void main(String[] args) {
-		carloan c=new carloan();  
+		Carloan c=new Carloan();  
 		c.IntrestCL();  
 		c.Intrest(); 
 
